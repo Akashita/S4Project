936a5b6b8a38900c2d0984317a0176129c3362cc
smookiPenguin <52562277+smookiPenguin@users.noreply.github.com>
2020-04-10 18:06:48 +0200

    test

--- ProjetS4/src/Model/Main.java
+++ ProjetS4/src/Model/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		e.nouvPersonne("planchamp", "damien");
 		e.nouvPersonne("planchamp", "pauline");
 		e.nouvSalle("Nivolet", 60);
-		e.nouvCalculateur("hal-9000");
+		e.nouvCalculateur("hal");
 		//e.creerActivite(e.getProjetSelectionner(), "activité 1", 11, 0, Temps.getAujourdhui());
 		//e.creerActivite(e.getProjetSelectionner(), "activité 2", 50, 1, Temps.getAujourdhui());
 	}
