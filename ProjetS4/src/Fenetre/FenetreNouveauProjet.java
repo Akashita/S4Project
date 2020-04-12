f4830aea63654c7ccef088b2dcca32c16c8a6fc3
smookiPenguin <52562277+smookiPenguin@users.noreply.github.com>
2020-04-12 04:56:22 +0200

    forgot

--- ProjetS4/src/Fenetre/FenetreNouveauProjet.java
+++ ProjetS4/src/Fenetre/FenetreNouveauProjet.java
@@ -13,93 +13,10 @@ import Panel_Fenetre.PanelNouveauProjet;
  */
 public class FenetreNouveauProjet extends FenetreModal{
 
-		/*
-		 * retourne un panel avec le champs de saisie et un label
-		 */
-	    JPanel panel = new JPanel();
-	    panel.setLayout(new GridLayout(1, 1));
-	    if (type == "nom") {
-		    panel.add(new JLabel("Veillez indiquer le nom du projet : "));
-	    	nom = new JTextField();
-		    panel.add(nom);	    	
-	    }
-	    if (type == "priorite") {
-		    panel.add(new JLabel("Veillez indiquez la priorité du projet: "));
-		    priorite = new JTextField();
-		    panel.add(priorite);	    	
-	    }
-		return panel;
-	}
-	
-	@Override
-	public JPanel ajoutBouton() {
-		JPanel panel = new JPanel();
-		panel.setSize(this.getWidth(),10);
-		panel.add(creerBouttonAnnuler());
-		panel.add(creerBouttonAjout());
-		return panel;
-	}
 	
 	/**
 	 * 
-	 * @param type
-	 * @return le bouton qui permet l'action pour creer la nouvelle activité
 	 */
-	private JButton creerBouttonAjout() {
-		JButton bouton = new JButton("Creer");
-	    bouton.addActionListener(new ActionListener() {  
-	        public void actionPerformed(ActionEvent e) {
-	        	creerProjet();	 
-	        }
-	    });			
-	    return bouton;
-	}
-	
-	/**
-	 * 
-	 * @return le bouton qui permet l'action pour fermer la fenetre
-	 */
-	private JButton creerBouttonAnnuler() {
-		JButton bouton = new JButton("Annuler");
-	    bouton.addActionListener(new ActionListener() {  
-	        public void actionPerformed(ActionEvent e) {
-	        	dispose();
-	        }
-	    });			
-	    return bouton;
-	}
-	
-	
-	
-	/**
-	 * verifie les informations saisie et ajoute le projet au model (Entreprise)
-	 */
-	private void creerProjet() {
-		if (!nom.getText().isEmpty()) {
-			if (!priorite.getText().isEmpty()) {
-				if (estUnEntier(priorite.getText())) {
-					int index = comboBoxPersonne.getSelectedIndex();
-				    Ressource ressource = listePersonne.get(index);
-					entreprise.creerProjet((Personne)ressource, nom.getText(), Integer.parseInt(priorite.getText()));
-					dispose();
-				}
-				else {
-			    	JOptionPane.showMessageDialog(null, "Veillez ecrire un nombre pour charge", "Erreur", JOptionPane.ERROR_MESSAGE);			
-				}
-			}
-			else {
-		    	JOptionPane.showMessageDialog(null, "Veillez ecrire sa priorité", "Erreur", JOptionPane.ERROR_MESSAGE);			
-			}
-		}
-		else {
-	    	JOptionPane.showMessageDialog(null, "Veillez ecrire son nom", "Erreur", JOptionPane.ERROR_MESSAGE);			
-		}
-	}
-	
-	private boolean estUnEntier(String chaine) {
-		try {
-			Integer.parseInt(chaine);
-		} catch (NumberFormatException e){
 	private static final long serialVersionUID = 1L;
 
 	public FenetreNouveauProjet(Entreprise entreprise) {
--- ProjetS4/src/Fenetre/FenetreNouvelleActivite.java
+++ ProjetS4/src/Fenetre/FenetreNouvelleActivite.java
@@ -12,194 +12,20 @@ import Panel_Fenetre.PanelNouvelleActivite;
  * @author damien planchamp
  *
  */
-		panelPrincipal.setBackground(Color.WHITE);
-		
-		panelPrincipal.add(infoAEcrire());
-		panelPrincipal.add(panelDebut());
-		
-		
-		/*panelPrincipal.add(creerBouttonAnnuler());
-		panelPrincipal.add(creerBouttonAjout());*/
-		panelPrincipal.add(ajoutBouton());
-		
-		
-		this.add(panelPrincipal);
-		this.revalidate();
-	}
-
-	private JPanel infoAEcrire() {
-		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
-	    panel.setBorder(BorderFactory.createTitledBorder("Rentrez les informations de l'activité"));
-		panel.add(creerJTextField("titre"));
-		panel.add(creerJTextField("charge"));
-		panel.add(creerJTextField("ordre"));
-
-		return panel;
-	}  
-    
-	private JPanel creerJTextField(String type) {
-		/*
-		 * retourne un panel avec le champs de saisie et un label
-		 */
-	    JPanel panel = new JPanel();
-	    panel.setLayout(new GridLayout(1, 1));
-	    if (type == "titre") {
-		    panel.add(new JLabel("Veillez indiquez le nom de l'activité : "));
-	    	titre = new JTextField();
-		    panel.add(titre);	    	
-	    }
-	    if (type == "charge") {
-		    panel.add(new JLabel("Veillez indiquez la charge de travail en jour/homme : "));
-	    	charge = new JTextField();
-		    panel.add(charge);	    	
-	    }
-	    if (type == "ordre") {
-		    panel.add(new JLabel("Veillez indiquez l'ordre (A étant le premier ordre): "));
-		    ArrayList<Activite> lAct = projet.getListe();
-		    String [] tAct = new String [lAct.size()];
-		    for (int i=0; i<lAct.size(); i++) {
-		    	tAct[i] = lAct.get(i).getTitre();
-		    }
-		    ordre = new JComboBox<String>(tAct);
-		    ordre.setSelectedIndex(tAct.length-1);
-		    panel.add(ordre);
-		    panel.add(checkAvant);
-	    }
-		return panel;
-	}
+public class FenetreNouvelleActivite extends FenetreModal{
 
 	
-	private JPanel panelDebut() {
-		JPanel panel = new JPanel();
-	    panel.setBorder(BorderFactory.createTitledBorder("Indiquez quand commencera l'activité"));
-
-	    String[] jours = {"01", "02", "03", "04", "05", "06", "07", "08", "09", "10"
-	    		, "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"
-	    		, "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
-	    String[] mois = {"Janvier", "Février", "Mars", "Avril",
-	    		"Mai", "Juin", "Juillet", "Aout",
-	    		"Septembre", "Octobre", "Novembre", "Décembre"};
-	    String[] annees = {"2020", "2021", "2022", "2023"};
-	    
-	    jour = new JComboBox<String>(jours);
-	    jour.setSelectedIndex(Temps.getAujourdhui().getDayOfMonth()-1);;
-	    this.mois = new JComboBox<String>(mois);
-	    this.mois.setSelectedIndex(Temps.getAujourdhui().getMonthValue()-1);;
-	    annee = new JComboBox<String>(annees);
-
-	    panel.add(jour);
-	    panel.add(this.mois);
-	    panel.add(annee);
-	    
-		return panel;
-		
-	}
-	
-	@Override
-	public JPanel ajoutBouton() {
-		JPanel panel = new JPanel();
-		panel.setSize(this.getWidth(),10);
-		panel.add(creerBoutonAnnuler());
-		panel.add(creerBoutonFin());
-		return panel;
-	}
-	
-
-	
-	
-	
-	/**
-	 * verifie les informations saisie et ajoute la ressource au model (Entreprise)
-	 */
-	private void creertActivite() {
-		if (!titre.getText().isEmpty()) {
-			if (!charge.getText().isEmpty()) {
-				if (estUnEntier(charge.getText())) {
-						LocalDate debut = creerLaDate();
-						int ordre = this.ordre.getSelectedIndex()+1;
-						if (checkAvant.isSelected()) {
-							ordre --;
-						}
-						if (ordre < 0) {
-							ordre = 0;
-						}
-						entreprise.creerActivite(projet, 
-								titre.getText(), Integer.parseInt(charge.getText()), ordre, debut);
-						dispose();
-				}
-				else {
-			    	JOptionPane.showMessageDialog(null, "Veillez ecrire un nombre pour charge", "Erreur", JOptionPane.ERROR_MESSAGE);			
-				}
-			}
-			else {
-		    	JOptionPane.showMessageDialog(null, "Veillez ecrire sa charge", "Erreur", JOptionPane.ERROR_MESSAGE);			
-			}
-		}
-		else {
-	    	JOptionPane.showMessageDialog(null, "Veillez ecrire son titre", "Erreur", JOptionPane.ERROR_MESSAGE);			
-		}
-	}
-	
-	private LocalDate creerLaDate() {
-		int jour = Integer.parseInt((String) this.jour.getSelectedItem());
-		int mois = this.mois.getSelectedIndex()+1;
-		int annee = Integer.parseInt((String) this.annee.getSelectedItem());
-		LocalDate debut = null;
-		if (Temps.jourValide (jour, mois, annee)) {
-			debut = LocalDate.of(annee, mois, jour);
-		}
-		else {
-	    	JOptionPane.showMessageDialog(null, "Date invalide", "Erreur", JOptionPane.ERROR_MESSAGE);			
-		}
-		
-		return debut;
-	}
-	
-
-	private boolean estUnEntier(String chaine) {
-		try {
-			Integer.parseInt(chaine);
-		} catch (NumberFormatException e){
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public void actionBoutonFin() {
-		// TODO Auto-generated method stub
-		
-	}
-
 	/**
 	 * 
-	 * @return le bouton qui permet l'action pour creer la nouvelle activité
 	 */
-	@Override
-	public JButton creerBoutonFin() {
-		JButton bouton = new JButton("Creer");
-	    bouton.addActionListener(new ActionListener() {  
-	        public void actionPerformed(ActionEvent e) {
-	        	creertActivite();	 
-	        }
-	    });			
-	    return bouton;
-	}
+	private static final long serialVersionUID = 1L;
 
-	/**
-	 * 
-	 * @return le bouton qui permet l'action pour fermer la fenetre
-	 */
-	@Override
-	public JButton creerBoutonAnnuler() {
-		JButton bouton = new JButton("Annuler");
-	    bouton.addActionListener(new ActionListener() {  
-	        public void actionPerformed(ActionEvent e) {
-	        	dispose();
-	        }
-	    });			
-	    return bouton;
+	public FenetreNouvelleActivite(Entreprise entreprise) {
+		super(entreprise, "Creation activite", 400, 300);
+		panelInterface = new PanelNouvelleActivite(entreprise, this);
+		this.setLayout(new BorderLayout());
+		this.add(panelInterface, BorderLayout.CENTER);
+		this.setResizable(false);
+		this.setVisible(true);
 	}
 }
