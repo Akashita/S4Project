8a77bb680fb4f555f15649fbee96659b943958af
smookiPenguin <52562277+smookiPenguin@users.noreply.github.com>
2020-04-12 22:16:31 +0200

    nettoyage

---
+++ ProjetS4/src/EcouteurEvenement/MenuBarListener.java
@@ -0,0 +1,61 @@
+package EcouteurEvenement;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import Fenetre.FenetreModal;
+import Fenetre.FenetrePrincipale;
+import Model.Entreprise;
+
+
+/**
+ * Cette classe permet l'interaction utilisateur machine avec la barre du menu
+ * 
+ * @author damien planchamp
+ *
+ */
+
+public class MenuBarListener implements ActionListener{
+	Entreprise entreprise;
+	int choix;
+	
+	public MenuBarListener(Entreprise entreprise, int choix) {
+		this.entreprise = entreprise;
+		this.choix = choix;
+	}
+	
+	/**
+	 * creer une fenetre qui laissera le choix à l'utlisateur
+	 * entre la creation d'une ressource ou d'un projet
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (choix == FenetrePrincipale.NouveauProjet || choix == FenetrePrincipale.NouvelleRessource) {
+			creerFenetre();
+		}
+		
+		if (choix == FenetrePrincipale.NouvelleActivite || choix == FenetrePrincipale.ModifierProjet) {
+			if (entreprise.getProjetSelectionner() != null) {
+				creerFenetre();
+			}
+			else {
+		    	JOptionPane.showMessageDialog(null, "Aucun projet selectionné", "Erreur", JOptionPane.ERROR_MESSAGE);			
+			}
+		}
+
+		if (choix == FenetrePrincipale.ModifierActivite || choix == FenetrePrincipale.AjouterRessource || choix == FenetrePrincipale.EnleverRessource) {
+			if (entreprise.getActiviteSelectionner() != null) {
+				creerFenetre();
+			}
+			else {
+		    	JOptionPane.showMessageDialog(null, "Aucun activité selectionnée", "Erreur", JOptionPane.ERROR_MESSAGE);			
+			}
+		}
+	}
+	
+	private void creerFenetre() {
+		new FenetreModal(entreprise, choix);
+	}
+}
