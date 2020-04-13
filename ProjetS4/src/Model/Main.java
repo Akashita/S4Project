973af16c97faf5d5c138b6f50d6ddabd88a5165b
Magira73 <silvio_dompnier@hotmail.fr>
2020-04-13 18:09:58 +0200

    main test + class recupinfo

--- ProjetS4/src/Model/Main.java
+++ ProjetS4/src/Model/Main.java
@@ -1,5 +1,9 @@
 package Model;
 
+import java.sql.*;
+import java.util.ArrayList;
+
+import SQL.*;
 
 public class Main {
 	public static void main (String[] args) {
@@ -11,5 +15,29 @@ public class Main {
 		e.nouvCalculateur("hal");
 		//e.creerActivite(e.getProjetSelectionner(), "activité 1", 11, 0, Temps.getAujourdhui());
 		//e.creerActivite(e.getProjetSelectionner(), "activité 2", 50, 1, Temps.getAujourdhui());
+		ArrayList<String> testP = new ArrayList<String>();
+		testP.add("sql");
+		ArrayList<Integer> testN = new ArrayList<Integer>();
+		testN.add(1);
+		JavaSQLPersonne testSqlPersonne = new JavaSQLPersonne("Geyer","Jules","Chef","putheu", testP, testN);
+		JavaSQLDomaine testSqlDomaine = new JavaSQLDomaine("sqli");
+		JavaSQLCompetence testSqlCompetence = new JavaSQLCompetence(10,"xjbv",5);
+		
+//		try {
+////			testSqlDomaine.creation();
+////			testSqlDomaine.insertion();
+////			
+////			testSqlDomaine.affiche();
+//			
+////			testSqlCompetence.creation();
+//			testSqlPersonne.creation();
+////			
+////			testSqlPersonne.insertion();
+////			testSqlPersonne.drop();
+//			testSqlPersonne.affiche();
+//
+//		}catch(SQLException f){
+//			f.printStackTrace();
+//		}
 	}
 }
---
+++ ProjetS4/src/SQL/RecupInfoBDD.java
@@ -0,0 +1,63 @@
+package SQL;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import Model.Activite;
+import Model.Entreprise;
+import Model.Projet;
+import Ressource.Calculateur;
+import Ressource.Personne;
+import Ressource.Salle;
+
+public final class RecupInfoBDD {
+	static JavaSQLDomaine domaine = new JavaSQLDomaine();
+	static JavaSQLPersonne personne = new JavaSQLPersonne();
+	static JavaSQLCalculateur calculateur = new JavaSQLCalculateur();
+	static JavaSQLSalle salle = new JavaSQLSalle();
+	static JavaSQLActivite activite = new JavaSQLActivite();
+	static JavaSQLProjet projet = new JavaSQLProjet();
+
+	public static void recupBDDDomaine(Entreprise e) throws SQLException {
+		ArrayList<String> dom = new ArrayList<String>();
+		dom = domaine.affiche();
+		for (int i = 0; i < dom.size(); i++) {
+			e.nouvDomaine(dom.get(i));
+		}
+	}
+
+	public static void recupBDDRessource(Entreprise e) throws SQLException {
+		ArrayList<Personne> pers = new ArrayList<Personne>();
+		pers = personne.affiche();
+		ArrayList<Calculateur> cal = new ArrayList<Calculateur>();
+		cal = calculateur.affiche();
+		ArrayList<Salle> sal= new ArrayList<Salle>();
+		sal = salle.affiche();
+		for (int i = 0; i < pers.size(); i++) {
+			e.nouvPersonne(pers.get(i));
+		}
+		for (int i = 0; i < cal.size(); i++) {
+			e.nouvCalculateur(cal.get(i));
+		}
+		for (int i = 0; i < sal.size(); i++) {
+			e.nouvSalle(sal.get(i));
+		}
+		
+	}
+	
+	public static void recupBDDActivite(Entreprise e) throws SQLException {
+		ArrayList<Activite> act = new ArrayList<Activite>();
+		act = activite.affiche();
+		for (int i = 0; i < act.size(); i++) {
+			e.nouvActivite(act.get(i));
+		}
+	}
+	
+	public static void recupBDDProjet(Entreprise e) throws SQLException {
+		ArrayList<Projet> prog = new ArrayList<Projet>();
+		prog = projet.affiche();
+		for (int i = 0; i < prog.size(); i++) {
+			e.nouvProjet(prog.get(i));
+		}
+	}
+}
