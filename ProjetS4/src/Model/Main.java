a98eb21c2e4a4a5f79333eec40ecf9a3b0921c59
Magira73 <silvio_dompnier@hotmail.fr>
2020-04-13 22:59:57 +0200

    corection bug

--- ProjetS4/src/Model/Main.java
+++ ProjetS4/src/Model/Main.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import SQL.*;
@@ -12,34 +13,33 @@ public class Main {
 		//e.creerActivite(e.getProjetSelectionner(), "activité 1", 11, 0, Temps.getAujourdhui());
 		//e.creerActivite(e.getProjetSelectionner(), "activité 2", 50, 1, Temps.getAujourdhui());
 		ArrayList<String> testP = new ArrayList<String>();
-		testP.add("sql");
+		testP.add("php");
 		ArrayList<Integer> testN = new ArrayList<Integer>();
 		testN.add(1);
 		JavaSQLPersonne testSqlPersonne = new JavaSQLPersonne("Geyer","Jules","Chef","putheu", testP, testN);
-		JavaSQLDomaine testSqlDomaine = new JavaSQLDomaine("sqli");
+		JavaSQLDomaine testSqlDomaine = new JavaSQLDomaine("php");
 		JavaSQLCompetence testSqlCompetence = new JavaSQLCompetence(10,"xjbv",5);
 		JavaSQLDebug test = new JavaSQLDebug();
 		JavaSQL java = new JavaSQL();
+		LocalDate  date = LocalDate.of(2000, 05, 28);
+		JavaSQLProjet testprojet = new JavaSQLProjet("projet test", 5, date, "rouge", 1);
 		
-//		try {
-////			testSqlDomaine.creation();
-////			testSqlDomaine.insertion();
-////			
-////			testSqlDomaine.affiche();
-//			
-////			testSqlCompetence.creation();
-////			testSqlPersonne.creation();
-////			
-////			testSqlPersonne.insertion();
-////			testSqlPersonne.drop();
-////			testSqlPersonne.affiche();
-//			
-////			test.affiche();
-////			test.drop();
-////			java.creation();
-//
-//		}catch(SQLException f){
-//			f.printStackTrace();
-//		}
+		
+		try {
+			testSqlDomaine.insertion();
+
+			testSqlPersonne.insertion();
+			
+			testprojet.insertion();
+			
+//			test.affiche();
+//			test.drop();
+//			java.creation();
+//			test.affiche();
+			
+
+		}catch(SQLException f){
+			f.printStackTrace();
+		}
 	}
 }
