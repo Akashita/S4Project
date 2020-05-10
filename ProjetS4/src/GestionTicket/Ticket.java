bcc34eb6c964a28cd74415eb9c06b78b8dd6051c
ElRoulioKFC <56266482+ElRoulioKFC@users.noreply.github.com>
2020-05-10 18:50:57 +0200

    up

--- ProjetS4/src/GestionTicket/Ticket.java
+++ ProjetS4/src/GestionTicket/Ticket.java
@@ -5,16 +5,28 @@ public class Ticket {
 	private String sujet;
 	private String message;
 	private String modif;
+	private int idEnvoyeur;
+	private int idReceveur;
 
-	public Ticket(int id, String sujet,String message, String modif) {
+	public Ticket(int id, String sujet,String message, String modif, int idEnvoyeur, int idReceveur) {
 		this.id = id;
 		this.sujet = sujet;
 		this.message = message;
 		this.modif = "PAS ENCORE FAIT";
+		this.idEnvoyeur = idEnvoyeur;
+		this.idReceveur = idReceveur;
 	}
 	
 	public int getId() {
 		return this.id;
+	}
+	
+	public int getIdEnvoyeur() {
+		return this.idEnvoyeur;
+	}
+	
+	public int getIdReceveur() {
+		return this.idReceveur;
 	}
 	
 	public String getSujet() {
